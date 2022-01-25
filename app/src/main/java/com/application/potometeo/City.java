4bc9f3d1b9e371c75fd3bc4d28766e6f80b57f45
tguegan <thomas.guegan@universite-paris-saclay.fr>
2022-01-25 16:54:26 +0100

    Added Search Class for JSON parsing

---
+++ app/src/main/java/com/application/potometeo/City.java
@@ -0,0 +1,24 @@
+package com.application.potometeo;
+
+public class City {
+
+    private String id, name, country;
+
+    public City(String id, String name, String country) {
+        this.id = id;
+        this.name = name;
+        this.country = country;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+}
