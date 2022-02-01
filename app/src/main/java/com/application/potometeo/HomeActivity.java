486163395e5912dcaadbb450a175ed631528a444
ajust <alexandre.just@universite-paris-saclay.fr>
2022-02-01 16:20:36 +0100

    Bijour²

--- app/src/main/java/com/application/potometeo/HomeActivity.java
+++ app/src/main/java/com/application/potometeo/HomeActivity.java
@@ -1,17 +1,36 @@
 package com.application.potometeo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
+
+import java.io.File;
 
 public class HomeActivity extends AppCompatActivity {
+    int temperature;
+    String var;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        temperature = 0;
+        Integer.parseInt(var);
+        ConstraintLayout ll = findViewById(R.id.ActivityHome);
+
+        if (temperature<10){
+            ll.setBackgroundResource(R.drawable.cold);
+        }
+        else if (temperature>25){
+            ll.setBackgroundResource(R.drawable.hot);
+        }
+        else{
+            ll.setBackgroundResource(R.drawable.mid);
+        }
     }
     public void ParamClicked(View view) {
 
