79441a74261dca2e9befabc48ac331e006ce4735
florent.weltmann <florent.weltmann@universite-paris-saclay.fr>
2022-02-07 01:24:35 +0100

    Add more cities and design impproved for the starting screen.

--- app/src/main/java/com/application/potometeo/StartActivity.java
+++ app/src/main/java/com/application/potometeo/StartActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
